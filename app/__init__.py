06cdee07ba96d833394050f32674720eb84ac4ed
karomag <roma-novikov@mail.ru>
2020-09-11 23:06:12 +0300

    Implemented Bootstrap.

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,9 @@
 # -*- coding:utf-8 -*-
 
-"""Экземпляр класса Flask.
-
-Сценарий выше просто создает объект приложения как экземпляр класса Flask,
-импортированного из пакета flask. Переменная __name__, переданная в класс
-Flask, является предопределенной переменной Python, которая задается именем
-модуля, в котором она используется. Один из аспектов, который может показаться
-запутанным вначале, состоит в том, что существуют два объекта с именем app.
-Пакет приложения определяется каталогом приложения и сценарием __init__.py и
-указан в инструкции routes импорта приложения. Переменная app определяется как
-экземпляр класса Flask в сценарии __init__.py, что делает его частью пакета
-приложения.
-"""
+"""Package app (Flask)."""
 
 from flask import Flask
+from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
@@ -28,5 +18,6 @@ migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
+boostrap = Bootstrap(app)
 
 from app import errors, models, routes  # noqa: F401 It is ok in flask
--- config.py
+++ config.py
@@ -24,4 +24,4 @@ class Config(object):
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['novikov.r31@gmail.com']
 
-    POSTS_PER_PAGE = 25
+    POSTS_PER_PAGE = 3
